f521fe95c5e6777fa90e5131007fe0a28bd50d1a
jose aquafonte <jmfls@iscte.pt>
2021-03-22 02:13:12 +0000

    Leitor de projeto e criador de packages 21/03

--- Proj/src/main/java/com/poortoys/examples/Package.java
+++ Proj/src/main/java/com/poortoys/examples/Package.java
@@ -1,13 +1,36 @@
 package com.poortoys.examples;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.io.FilenameUtils;
 
 public class Package {
 	
+	private String name;
+	private ArrayList<Class> arrayClasses;
+	
+	public Package(String name,File file) {
+		this.name=name;
+		System.out.println(name);
+		arrayClasses = new ArrayList<Class>();
+	
+		List<File> files= Arrays.asList(file.listFiles()).stream().filter(x->FilenameUtils.getExtension(x.getAbsolutePath()).equals("java")).collect(Collectors.toList());
+		for(File x: files) {
+			//criar classe com file e adicionar a arrayclasses
+		}
+		
+	}
+	
+	
+	
+	//
 	
 	public String getName_Package() {
-		return "";
+		return name;
 	}
 	
 	public List<Class> getClass_list(){
