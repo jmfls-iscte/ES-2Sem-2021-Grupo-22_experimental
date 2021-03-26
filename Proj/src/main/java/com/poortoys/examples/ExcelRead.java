dbdfcdcbcdb14d0e58f90ad44f1d105bdef471aa
Tiago Alves <tsasa@iscte.iul.pt>
2021-03-26 12:10:43 +0000

    Ações separadas por métodos mais pequenos

--- Proj/src/main/java/com/poortoys/examples/ExcelRead.java
+++ Proj/src/main/java/com/poortoys/examples/ExcelRead.java
@@ -57,6 +57,13 @@ public class ExcelRead {
 		codeSmells_Method = new Method().get_name_code_Smells();
 	}
 
+	public void ClearVars() {
+		currentCellInt = 0;
+		currentClass = null;
+		currentPackage = null;
+		currentMethod = null;
+	}
+
 	public void ReadFile() {
 
 		try {
@@ -70,10 +77,7 @@ public class ExcelRead {
 				Row currentRow = iterator.next();
 				Iterator<Cell> cellIterator = currentRow.iterator();
 
-				currentCellInt = 0;
-				currentClass = null;
-				currentPackage = null;
-				currentMethod = null;
+				ClearVars();
 
 				while (cellIterator.hasNext()) {
 
@@ -87,90 +91,16 @@ public class ExcelRead {
 						 */
 						cells.add(currentCell.getStringCellValue());
 					} else {
-
-						switch (currentCellInt) {
-						case 0:
-							// Method id é para ignorar?
-							break;
-						case 1:
-							// criar ou verificar se existe package com o nome atual da célula
-							String package_name = currentCell.getStringCellValue();
-							for (Package p : packages) {
-								if (p.getName_Package() == package_name)
-									currentPackage = p;
-							}
-
-							if (currentPackage == null) {
-								currentPackage = new Package(package_name);
-								packages.add(currentPackage);
-							}
-							break;
-
-						case 2:
-							// criar ou verificar se existe classe com o nome atual da célula
-							String class_name = currentCell.getStringCellValue();
-							currentClass = currentPackage.get_ClassByName(class_name);
-							if (currentClass == null) {
-								currentClass = new Class(class_name);
-								currentPackage.addClass(currentClass);
-							}
-							break;
-
-						case 3:
-							// criar ou verificar se existe metodo com o nome atual da célula
-
-							String method_name = currentCell.getStringCellValue();
-							currentMethod = currentClass.get_MethodByName(method_name);
-							if (currentMethod == null) {
-								currentMethod = new Method(method_name);
-								currentClass.addMethod(currentMethod);
-							}
-							break;
-
-						default:
-							/*
-							 * verificar em que metrica estou com ajuda do array String metricaAtual =
-							 * cells.get(currentCell);
-							 * 
-							 * Entrar no package Entrar na classe Entrar no metodo inserir metrica + valor
-							 */
-
-							String var = cells.get(currentCellInt);
-							switch (var) {
-							case "NOM_class":
-								if (currentClass.getNOM_class() == 0)
-									currentClass.setNOM_class((int) currentCell.getNumericCellValue());
-								break;
-							case "LOC_class":
-								if (currentClass.getLOC_class() == 0)
-									currentClass.setLOC_class((int) currentCell.getNumericCellValue());
-								break;
-							case "WMC_class":
-								if (currentClass.getWMC_class() == 0)
-									currentClass.setWMC_class((int) currentCell.getNumericCellValue());
-								break;
-							case "LOC_method":
-								if (currentMethod.getLOC_method() == 0)
-									currentMethod.setLOC_method((int) currentCell.getNumericCellValue());
-								break;
-							case "CYCLO_method":
-								if (currentMethod.getCYCLO_method() == 0)
-									currentMethod.setCYCLO_method((int) currentCell.getNumericCellValue());
-								break;
-
-							default:
-								break;
-							}
-
-							break;
-						}
-
+						
+						ChooseCell(currentCell);
 						currentCellInt++;
-					}
-				}
+					} // Fim do else
+					
+				} // Fim do while das celulas
+				
 				n_reads++;
 
-			}
+			}//Fim do while das linhas
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -179,6 +109,92 @@ public class ExcelRead {
 
 		System.out.print("So para parar");
 
+	}
+
+	public void ChooseMetric(String var, Cell currentCell) {
+		switch (var) {
+		case "NOM_class":
+			if (currentClass.getNOM_class() == 0)
+				currentClass.setNOM_class((int) currentCell.getNumericCellValue());
+			break;
+		case "LOC_class":
+			if (currentClass.getLOC_class() == 0)
+				currentClass.setLOC_class((int) currentCell.getNumericCellValue());
+			break;
+		case "WMC_class":
+			if (currentClass.getWMC_class() == 0)
+				currentClass.setWMC_class((int) currentCell.getNumericCellValue());
+			break;
+		case "LOC_method":
+			if (currentMethod.getLOC_method() == 0)
+				currentMethod.setLOC_method((int) currentCell.getNumericCellValue());
+			break;
+		case "CYCLO_method":
+			if (currentMethod.getCYCLO_method() == 0)
+				currentMethod.setCYCLO_method((int) currentCell.getNumericCellValue());
+			break;
+
+		default:
+			break;
+		}
+
+	}
+	
+	
+	
+	public void ChooseCell(Cell currentCell)
+	{
+		switch (currentCellInt) {
+		case 0:
+			// Method id é para ignorar?
+			break;
+		case 1:
+			// criar ou verificar se existe package com o nome atual da célula
+			String package_name = currentCell.getStringCellValue();
+			for (Package p : packages) {
+				if (p.getName_Package() == package_name)
+					currentPackage = p;
+			}
+
+			if (currentPackage == null) {
+				currentPackage = new Package(package_name);
+				packages.add(currentPackage);
+			}
+			break;
+
+		case 2:
+			// criar ou verificar se existe classe com o nome atual da célula
+			String class_name = currentCell.getStringCellValue();
+			currentClass = currentPackage.get_ClassByName(class_name);
+			if (currentClass == null) {
+				currentClass = new Class(class_name);
+				currentPackage.addClass(currentClass);
+			}
+			break;
+
+		case 3:
+			// criar ou verificar se existe metodo com o nome atual da célula
+
+			String method_name = currentCell.getStringCellValue();
+			currentMethod = currentClass.get_MethodByName(method_name);
+			if (currentMethod == null) {
+				currentMethod = new Method(method_name);
+				currentClass.addMethod(currentMethod);
+			}
+			break;
+
+		default:
+			/*
+			 * verificar em que metrica estou com ajuda do array String metricaAtual =
+			 * cells.get(currentCell);
+			 * 
+			 * Entrar no package Entrar na classe Entrar no metodo inserir metrica + valor
+			 */
+
+			String var = cells.get(currentCellInt);
+			ChooseMetric(var, currentCell);
+			break;
+		} // Fim switch
 	}
 
 }
